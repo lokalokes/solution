0a3db81aa378de53b7c592ebf5290ca598443f3c
lokalokes <47306649+lokalokes@users.noreply.github.com>
2020-12-26 10:57:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,3 +25,14 @@ def solution(lt=list):
         if answer:
             return answer
         else: return  answer
+
+def solution(a=list):
+    n=0
+    mis=0
+    for i in a:
+        n+=1
+    lt=[i for i in range(n+1)]
+    for i in lt:
+        if i not in a:
+            mis=i
+    return mis
